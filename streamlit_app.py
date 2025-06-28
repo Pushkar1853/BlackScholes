368f392aabfb1873800683fd4ead434202576942
Pushkar Ambastha <105978655+Pushkar1853@users.noreply.github.com>
2025-06-29 02:45:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -116,8 +116,8 @@ with st.sidebar:
     st.title("📈 $Black Scholes Model$")
     st.write("`Created by:`")
     linkedin_url = 'https://www.linkedin.com/in/pushkar-ambastha/'
-    st.markdown(f"<a href="{linkedin_url}" target="_blank" style="text-decoration:none; color: inherit;"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;">`Pushkar Ambastha`</a>",unsafe_allow_html=True) 
-
+    st.markdown(f'<a href="{linkedin_url}" target="_blank" style="text-decoration: none; color: inherit;"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;">`Pushkar Ambastha`</a>',unsafe_allow_html=True) 
+    
     current_price = st.number_input("<i> Current  Asset  Price </i>  ($S_{t}$)", value = 100.0)
     strike = st.number_input(" Strike  Price  (K)", value=100.0)
     time_to_maturity = st.number_input("$Time  to  Maturity (t  in  Years)$", value=1.0)
