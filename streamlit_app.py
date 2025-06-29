8b216a6fe23b57314fd13665892cf4e2e6a0d4ba
Pushkar Ambastha <105978655+Pushkar1853@users.noreply.github.com>
2025-06-29 10:39:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -106,7 +106,7 @@ class BlackScholes:
 
         # Gamma
         self.call_gamma = norm.pdf(d1) / (
-            strike * volatility * sqrt(time_to_maturity)
+            current_price * volatility * sqrt(time_to_maturity)
         )
         self.put_gamma = self.call_gamma
 
@@ -114,22 +114,29 @@ class BlackScholes:
 
 with st.sidebar:
     st.title("📈 Black Scholes Model")
-    st.write("`Created by:`")
+    st.write('Created by:')
     linkedin_url = 'https://www.linkedin.com/in/pushkar-ambastha/'
-    st.markdown(f'<a href="{linkedin_url}" target="_blank" style="text-decoration: none; color: inherit;"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;">` Pushkar Ambastha `</a>',unsafe_allow_html=True) 
-    
-    current_price = st.number_input(" Current  Asset  Price  ($S_{t}$)", value = 100.0)
-    strike = st.number_input(" Strike  Price  (K)", value=100.0)
-    time_to_maturity = st.number_input("Time  to  Maturity (t  in  Years)", value=1.0)
-    volatility = st.number_input("Volatility  (σ)", value=0.2)
-    interest_rate = st.number_input("Risk - Free   Interest  Rate  (r)", value=0.05)
+    st.markdown(f'''
+    <a href="{linkedin_url}" target="_blank" style="text-decoration: none; display: inline-flex; align-items: center; gap: 8px;">
+        <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="20" height="20" style="margin: 0;" />
+        <span style="font-size: 16px; font-weight: normal;">Pushkar Ambastha</span>
+    </a>
+    ''', unsafe_allow_html=True)
+
+    current_price = st.number_input("Current Asset Price($S_{t}$)", value = 100.0)
+    strike = st.number_input("Strike Price (K)", value=100.0)
+    time_to_maturity = st.number_input("Time to Maturity (t)(Years)", value=1.0)
+    volatility = st.number_input("Volatility (σ)", value=0.2)
+    interest_rate = st.number_input("Risk-Free Interest Rate (r)", value=0.05)
+    call_purchase_price = st.number_input("Purchase Price of CALL Option", value=5.0)
+    put_purchase_price = st.number_input("Purchase Price of PUT Option", value=5.0)
 
     st.markdown('----')
-    calculate_btn = st.button('Heatmap  Parameters')
-    spot_min = st.number_input('Min  Spot  Price', min_value=0.01, value=current_price*0.8, step=0.01)
-    spot_max = st.number_input('Max  Spot  Price', min_value=0.01, value=current_price*1.2, step=0.01)
-    vol_min = st.slider('Min  Volatility  for  Heatmap', min_value=0.01, max_value=1.0, value=volatility*0.5, step=0.01)
-    vol_max = st.slider('Max  Volatility  for  Heatmap', min_value=0.01, max_value=1.0, value=volatility*1.5, step=0.01)
+    calculate_btn = st.button('Heatmap Parameters')
+    spot_min = st.number_input('Min Spot Price', min_value=0.01, value=current_price*0.8, step=0.01)
+    spot_max = st.number_input('Max Spot Price', min_value=0.01, value=current_price*1.2, step=0.01)
+    vol_min = st.slider('Min Volatility for Heatmap', min_value=0.01, max_value=1.0, value=volatility*0.5, step=0.01)
+    vol_max = st.slider('Max Volatility for Heatmap', min_value=0.01, max_value=1.0, value=volatility*1.5, step=0.01)
     
     spot_range = np.linspace(spot_min, spot_max, 10)
     vol_range = np.linspace(vol_min, vol_max, 10)
@@ -161,6 +168,38 @@ def plot_heatmap(bs_model, spot_range, vol_range, strike):
     fig_put, ax_put = plt.subplots(figsize=(10,8))
     sns.heatmap(put_prices, xticklabels=np.round(spot_range, 2), yticklabels=np.round(vol_range, 2), annot=True, fmt=".2f", cmap="RdYlGn", ax=ax_put)
     ax_put.set_title('PUT')
+    ax_put.set_xlabel('Spot Price')
+    ax_put.set_ylabel('Volatility')
+
+    return fig_call, fig_put
+
+def plot_pnl_heatmap(bs_model, spot_range, vol_range, strike, call_purchase_price, put_purchase_price):
+    call_pnl = np.zeros((len(vol_range), len(spot_range)))
+    put_pnl = np.zeros((len(vol_range), len(spot_range)))
+
+    for i, vol in enumerate(vol_range):
+        for j, spot in enumerate(spot_range):
+            bs_temp = BlackScholes(
+                time_to_maturity=bs_model.time_to_maturity,
+                strike=strike,
+                current_price=spot,
+                volatility=vol,
+                interest_rate=bs_model.interest_rate
+            )
+            bs_model.calculate_prices()
+            call_pnl[i,j] = bs_temp.call_price - call_purchase_price
+            put_pnl[i,j] = bs_temp.put_price - put_purchase_price
+    
+    fig_call, ax_call = plt.subplots(figsize = (10,8))
+    sns.heatmap(call_pnl, xticklabels=np.round(spot_range, 2), yticklabels=np.round(vol_range, 2),
+                annot=True, fmt=".2f", cmap="RdYlGn", center=0, ax=ax_call)
+    ax_call.set_title('CALL P&L')
+    ax_call.set_xlabel('Spot Price')
+    ax_call.set_ylabel('Volatility')
+
+    fig_put, ax_put = plt.subplots(figsize=(10,8))
+    sns.heatmap(put_pnl, xticklabels=np.round(spot_range, 2), yticklabels=np.round(vol_range, 2), annot=True, fmt=".2f", cmap='RdYlGn', center=0, ax=ax_put)
+    ax_put.set_title('PUT P&L')
     ax_put.set_xlabel('Spot Price')
     ax_put.set_ylabel('Volatility')
 
@@ -210,15 +249,31 @@ st.title("Options Price - Interactive Heatmap")
 st.info("Explore how option prices fluctuate with varying 'Spot Prices and Volatility' levels using interactive heatmap parameters, all while maintaining a constant 'Strike Price'.")
 
 # Interactive Sliders and Heatmaps for Call and Put Options
-col1, col2 = st.columns([1,1], gap="small")
+# col1, col2 = st.columns([1,1], gap="small")
+
+# with col1:
+#     st.subheader('Call Price Heatmap')
+#     heatmap_fig_call, _ = plot_heatmap(bs_model, spot_range, vol_range, strike)
+#     st.pyplot(heatmap_fig_call)
+
+# with col2:
+#     st.subheader('Put Price Heatmap')
+#     _, heatmap_fig_put = plot_heatmap(bs_model, spot_range, vol_range, strike)
+#     st.pyplot(heatmap_fig_put)
+
+st.markdown("## Profit & Loss Heatmap")
+st.markdown("Visual how your purchased CALL and PUT options perform under different market conditions.")
+
+col1, col2 = st.columns([1,1], gap='small')
 
 with col1:
-    st.subheader('Call Price Heatmap')
-    heatmap_fig_call, _ = plot_heatmap(bs_model, spot_range, vol_range, strike)
-    st.pyplot(heatmap_fig_call)
+    st.subheader("CALL Option P&L Heatmap")
+    pnl_fig_call, _ = plot_pnl_heatmap(bs_model, spot_range, vol_range, strike, call_purchase_price, put_purchase_price)
+    st.pyplot(pnl_fig_call)
 
 with col2:
-    st.subheader('Put Price Heatmap')
-    _, heatmap_fig_put = plot_heatmap(bs_model, spot_range, vol_range, strike)
-    st.pyplot(heatmap_fig_put)
+    st.subheader("PUT Option P&L Heatmap")
+    _, pnl_fig_put = plot_pnl_heatmap(bs_model, spot_range, vol_range, strike, call_purchase_price, put_purchase_price)
+    st.pyplot(pnl_fig_put)
+    
     
